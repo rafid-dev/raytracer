3f77c514e1794f690fbee02acda1f737875779d3
rafid-dev <vorahsan@gmail.com>
2024-03-30 00:08:23 +0600

    Make Vector class with all basic arithmatic operations

---
+++ src/data/vector.hpp
@@ -0,0 +1,181 @@
+#pragma once
+
+#include <algorithm>
+#include <array>
+#include <cassert>
+#include <functional>
+#include <initializer_list>
+
+namespace data {
+
+    template <typename TYPE, size_t SIZE>
+    class Vector : public std::array<TYPE, SIZE> {
+    private:
+        // Templated arithmetic operation for vector-vector operations
+        template <typename Function>
+        Vector<TYPE, SIZE> arithmetic_operation(const Vector<TYPE, SIZE>& other, Function op) const {
+            Vector<TYPE, SIZE> result;
+            std::transform(this->begin(), this->end(), other.begin(), result.begin(), op);
+            return result;
+        }
+
+        // Templated arithmetic operation for vector-scalar operations
+        template <typename Function>
+        Vector<TYPE, SIZE> arithmetic_operation(const TYPE val, Function op) const {
+            Vector<TYPE, SIZE> result;
+            result.fill(val);
+            std::transform(this->begin(), this->end(), result.begin(), result.begin(), op);
+            return result;
+        }
+
+    public:
+        Vector() : std::array<TYPE, SIZE>() {
+            static_assert(SIZE > 0);
+        };
+
+        template <typename... Args>
+        Vector(Args... args) {
+            static_assert(SIZE > 0);
+            static_assert(sizeof...(Args) == SIZE, "Number of arguments must match vector size.");
+
+            auto it_this = this->begin();
+            for (auto arg : {static_cast<TYPE>(args)...}) {
+                *it_this++ = arg;
+            }
+        }
+
+        auto operator()(size_t index) const {
+            return (*this)[index];
+        }
+
+        auto operator()(size_t index) {
+            return (*this)[index];
+        }
+
+        void operator=(const Vector<TYPE, SIZE>& other) {
+            std::copy(other.begin(), other.end(), this->begin());
+        }
+
+        /*/////////////////////////////////////////////
+            const vector-vector arithmetic overloads
+        *//////////////////////////////////////////////
+
+        auto operator+(const Vector<TYPE, SIZE>& other) const {
+            return arithmetic_operation(other, std::plus<TYPE>());
+        }
+        auto operator-(const Vector<TYPE, SIZE>& other) const {
+            return arithmetic_operation(other, std::minus<TYPE>());
+        }
+
+        auto operator*(const Vector<TYPE, SIZE>& other) const {
+            return arithmetic_operation(other, std::multiplies<TYPE>());
+        }
+
+        auto operator/(const Vector<TYPE, SIZE>& other) const {
+            return arithmetic_operation(other, std::divides<TYPE>());
+        }
+
+        /*//////////////////////////////////////////////
+            const vector-scalar arithmetic overloads
+        *//////////////////////////////////////////////
+        auto operator+(const TYPE val) const {
+            return arithmetic_operation(val, std::plus<TYPE>());
+        }
+        auto operator-(const TYPE val) const {
+            return arithmetic_operation(val, std::minus<TYPE>());
+        }
+
+        auto operator*(const TYPE val) const {
+            return arithmetic_operation(val, std::multiplies<TYPE>());
+        }
+
+        auto operator/(const TYPE val) const {
+            return arithmetic_operation(val, std::divides<TYPE>());
+        }
+
+        /*//////////////////////////////////////////////
+            vector-vector arithmetic assignment overloads
+        *//////////////////////////////////////////////
+        void operator+=(const Vector<TYPE, SIZE>& other) {
+            *this = *this + other;
+        }
+
+        void operator-=(const Vector<TYPE, SIZE>& other) {
+            *this = *this - other;
+        }
+
+        void operator*=(const Vector<TYPE, SIZE>& other) {
+            *this = *this * other;
+        }
+
+        void operator/=(const Vector<TYPE, SIZE>& other) {
+            *this = *this / other;
+        }
+
+        /*//////////////////////////////////////////////
+            vector-scalar arithmetic assignment overloads
+        *//////////////////////////////////////////////
+
+        void operator+=(const TYPE val) {
+            *this = *this + val;
+        }
+
+        void operator-=(const TYPE val) {
+            *this = *this - val;
+        }
+
+        void operator*=(const TYPE val) {
+            *this = *this * val;
+        }
+
+        void operator/=(const TYPE val) {
+            *this = *this / val;
+        }
+
+        /*//////////////////////////////////////////////
+            vector-vector comparison overloads
+        *//////////////////////////////////////////////
+
+        bool operator==(const Vector<TYPE, SIZE>& other) const {
+            return std::equal(this->begin(), this->end(), other.begin());
+        }
+
+        bool operator!=(const Vector<TYPE, SIZE>& other) const {
+            return !(*this == other);
+        }
+
+        /*//////////////////////////////////////////////
+            vector-scalar comparison overloads
+        *//////////////////////////////////////////////
+
+        bool operator==(const TYPE val) const {
+            return std::all_of(this->begin(), this->end(), [val](TYPE x) { return x == val; });
+        }
+
+        bool operator!=(const TYPE val) const {
+            return !(*this == val);
+        }
+
+        // Negation operator
+        Vector<TYPE, SIZE> operator-() const {
+            return *this * -1;
+        }
+
+        /*/////////////////////////
+            stdout overloads
+        */////////////////////////
+        friend inline std::ostream& operator<<(std::ostream& os, const Vector<TYPE, SIZE>& vec) {
+            os << "(";
+            for (size_t i = 0; i < SIZE; i++) {
+                os << vec[i];
+                if (i != SIZE - 1) {
+                    os << ", ";
+                }
+            }
+            os << ")";
+            return os;
+        }
+    };
+
+    using Vector3f = Vector<float, 3>;
+} // namespace data
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
+#include "data/vector.hpp"
 
 int main(int argc, char const *argv[])
 {
-    std::cout << "Hello world!" << std::endl;
+
+    data::Vector3f vec1 (2, 2, 2);
+    data::Vector3f vec2 (3, 3, 3);
+
+    std::cout << "vec1 = " << vec1 << std::endl;
+    std::cout << "vec2 = " << vec2 << std::endl;
+
+    // All vector-vector arithmatic operations printed
+    std::cout << "vec1 + vec2 = " << vec1 + vec2 << std::endl;
+    std::cout << "vec1 - vec2 = " << vec1 - vec2 << std::endl;
+    std::cout << "vec1 * vec2 = " << vec1 * vec2 << std::endl;
+    std::cout << "vec1 / vec2 = " << vec1 / vec2 << std::endl;
+
+    // All vector-scalar arithmatic operations printed
+    std::cout << "vec1 + 2 = " << vec1 + 2 << std::endl;
+    std::cout << "vec1 - 2 = " << vec1 - 2 << std::endl;
+    std::cout << "vec1 * 2 = " << vec1 * 2 << std::endl;
+    std::cout << "vec1 / 2 = " << vec1 / 2 << std::endl;
+
     return 0;
 }
